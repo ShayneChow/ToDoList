092acf3108e9a793c5174f285535c74caa921621
choushayne <Shayne@choushaynedeMac-mini-2.local>
2014-11-10 11:14:05 +0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by choushayne on 14/11/10.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by choushayne on 14/11/10.
+//  Copyright (c) 2014年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
