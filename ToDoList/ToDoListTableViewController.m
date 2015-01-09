ea9df7937437889ca12d89328a540ceb5a678db5
Shayne Chow <shayne@authbuy.org>
2015-01-09 18:18:21 +0800

    隐藏没有内容的cell，并将多余文件/注释删除

--- ToDoList/ToDoListTableViewController.m
+++ ToDoList/ToDoListTableViewController.m
@@ -20,15 +20,15 @@
 
 //创建几个列表项目，并将它们添加到数组
 - (void)loadInitialData {
-//    ToDoItem *item1 = [[ToDoItem alloc] init];
-//    item1.itemName = @"Buy milk";
-//    [self.toDoItems addObject:item1];
-//    ToDoItem *item2 = [[ToDoItem alloc] init];
-//    item2.itemName = @"Buy eggs";
-//    [self.toDoItems addObject:item2];
-//    ToDoItem *item3 = [[ToDoItem alloc] init];
-//    item3.itemName = @"Read a book";
-//    [self.toDoItems addObject:item3];
+    ToDoItem *item1 = [[ToDoItem alloc] init];
+    item1.itemName = @"Buy milk";
+    [self.toDoItems addObject:item1];
+    ToDoItem *item2 = [[ToDoItem alloc] init];
+    item2.itemName = @"Buy eggs";
+    [self.toDoItems addObject:item2];
+    ToDoItem *item3 = [[ToDoItem alloc] init];
+    item3.itemName = @"Read a book";
+    [self.toDoItems addObject:item3];
 }
 
 - (IBAction)unwindToList:(UIStoryboardSegue *)segue{
@@ -46,11 +46,6 @@
     self.toDoItems = [[NSMutableArray alloc] init];     //分配并初始化 toDoItems 数组
     [self loadInitialData];     //调用 loadInitialData
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -90,53 +85,10 @@
         cell.accessoryType = UITableViewCellAccessoryNone;
     }
     
+    self.tableView.tableFooterView = [[UIView alloc] init];     //隐藏没有内容的cell
+    
     return cell;
 }
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 #pragma mark - Table view delegate
 
